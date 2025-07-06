9b86d0db2cf4fac5ee97d52215f593d8561ccd36
Arun-Hegde <arunhegde697@gmail.com>
2025-07-06 11:38:31 +0530

    Update analytics_by_category.py

--- Frontend/analytics_by_category.py
+++ Frontend/analytics_by_category.py
@@ -6,35 +6,33 @@ import pandas as pd
 API_URL = 'http://localhost:8000'
 
 def analytics_by_category_tab():
-    col1,col2 = st.columns(2)
+    col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input('Start Date',datetime(2024,8,1))
-
+        start_date = st.date_input('Start Date', datetime(2024, 8, 1))
     with col2:
         end_date = st.date_input('End Date', datetime(2024, 8, 5))
 
-    st.button('Get Analytics')
-    if st.button:
-        payload ={
+    if st.button('Get Analytics'):
+        payload = {
             'start_date': start_date.strftime('%Y-%m-%d'),
             'end_date': end_date.strftime('%Y-%m-%d')
         }
-        response = requests.post(f'{API_URL}/analytics/category/',json=payload)
+        response = requests.post(f'{API_URL}/analytics/category/', json=payload)
         response = response.json()
 
         data = {
             'Category': list(response.keys()),
-            'Total':[response[category]['total'] for category in response],
-            'Percentage':[response[category]['percentage'] for category in response]
+            'Total': [response[cat]['total'] for cat in response],
+            'Percentage': [response[cat]['percentage'] for cat in response]
         }
+
         df = pd.DataFrame(data)
-        df_sorted = df.sort_values(by='Percentage',ascending=False)
+        df_sorted = df.sort_values(by='Percentage', ascending=False)
 
-        st.title('Expense Breakdown By Category')
+        st.markdown("<h3 style='color:#6A5ACD;'>Category-wise Expense Distribution</h3>", unsafe_allow_html=True)
+        st.bar_chart(data=df_sorted.set_index("Category")["Percentage"], use_container_width=True)
 
-        st.bar_chart(data=df_sorted.set_index("Category")['Percentage'],width=0,height=0,use_container_width=24)
-
-        df_sorted['Total']=df_sorted['Total'].map('{:.2f}'.format)
+        df_sorted['Total'] = df_sorted['Total'].map('{:.2f}'.format)
         df_sorted['Percentage'] = df_sorted['Percentage'].map('{:.2f}'.format)
 
-        st.table(df_sorted)
+        st.dataframe(df_sorted, use_container_width=True)
